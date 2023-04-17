d4ba97f438a297241ce565dfeaee39e299a57bd3
Angelica Agatha <125953295+angelicagatha@users.noreply.github.com>
2023-04-18 01:04:51 +0700

    Update koneksi.php

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -1,6 +1,6 @@
 <?php
 
-$sambung = mysqli_connect("localhost", "root", "", "test");
+$sambung = mysqli_connect("localhost", "root", "", "dbgereja");
 
 if (!$sambung) { // jika tidak terkoneksi maka menampilkan pesan error
     die("Gagal terhubung ke database : " . mysqli_connect_errno());
