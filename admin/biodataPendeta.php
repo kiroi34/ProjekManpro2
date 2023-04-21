adb7e793296b3b054af4755cef8c09b0ff3be31e
Alan Satria <alansatria6@gmail.com>
2023-04-21 20:30:48 +0700

    rapikan css

--- admin/biodataPendeta.php
+++ admin/biodataPendeta.php
@@ -20,7 +20,7 @@ require_once "connByAlan.php";
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <link rel="stylesheet" href="fa_icons/css/all.css"> 
-     <link rel="stylesheet" href="biodataPendeta.css"> 
+     <link rel="stylesheet" href="css/biodataPendeta.css"> 
      <style>
      </style>
       <?
