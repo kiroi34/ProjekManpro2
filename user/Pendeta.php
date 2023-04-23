306ceb8369195ef3d7ebc80d2f67e9056f0046c0
Alan Satria <alansatria6@gmail.com>
2023-04-24 01:02:41 +0700

    manggil pendeta ke tampilan user

--- user/Pendeta.php
+++ user/Pendeta.php
@@ -29,7 +29,7 @@ require_once "../admin/connByAlan.php";
           font-family: 'Poppins';font-size: 18px;
         }
 
-        @media (min-width : 768px) {
+        /* @media (min-width : 768px) {
           #countdownWrapper {
             margin-top: 0;
             position: absolute;
@@ -58,7 +58,192 @@ require_once "../admin/connByAlan.php";
         }
         .form-group.error input {
           border-color: #B30000;
-        }
+        } */
+
+        
+
+/* :root {
+  --purple: hsl(240, 80%, 89%);
+  --pink: hsl(0, 59%, 94%);
+  --light-bg: hsl(204, 37%, 92%);
+  --light-gray-bg: hsl(0, 0%, 94%);
+  --white: hsl(0, 0%, 100%);
+  --dark: hsl(0, 0%, 7%);
+  --text-gray: hsl(0, 0%, 30%);
+} */
+
+
+h3 {
+  font-size: 1.5em;
+  font-weight: 700;
+}
+
+p {
+  font-size: 1em;
+  line-height: 1.7;
+  font-weight: 300;
+  color: black;
+}
+
+.description {
+  white-space: wrap;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+.wrap {
+  display: flex;
+  align-items: stretch;
+  width: 100%;
+  gap: 24px;
+  padding: 24px;
+  flex-wrap: wrap;
+}
+
+.box {
+  display: flex;
+  flex-direction: column;
+  flex-basis: 100%;
+  position: relative;
+  padding: 24px;
+  background: rgb(300, 300, 300);
+  box-shadow: 5px 10px 8px #888888;
+  border: 1px solid;
+}
+
+.box-top {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  gap: 12px;
+  margin-bottom: 36px;
+}
+/* image ganti fill kalo emg gajelas  */
+.box-image {
+  width: 100%;
+  height: 360px;
+  object-fit: cover; 
+  object-position: 50% 20%;
+}
+
+.title-flex {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.box-title {
+  border-left: 3px  ;
+  padding-left: 12px;
+  text-align: center;
+}
+
+.user-follow-info {
+  color: rgb(47, 47, 47);
+  text-align: center;
+}
+
+.button {
+  display: block;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  margin-top: auto;
+  padding: 16px;
+  color: #000;
+  background: transparent;
+  box-shadow: 0px 0px 0px 1px black inset;
+  transition: background 0.4s ease;
+}
+
+.button:hover {
+  background: var(--purple);
+}
+
+.fill-one {
+  background: var(--light-bg);
+}
+
+.fill-two {
+  background: var(--pink);
+}
+
+/* RESPONSIVE QUERIES */
+
+@media (min-width: 320px) {
+  .title-flex {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: start;
+  }
+  .user-follow-info {
+    margin-top: 6px;
+  }
+}
+
+@media (min-width: 460px) {
+  .title-flex {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: start;
+  }
+  .user-follow-info {
+    margin-top: 6px;
+  }
+}
+
+@media (min-width: 640px) {
+  .box {
+    flex-basis: calc(50% - 12px);
+  }
+  .title-flex {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: start;
+  }
+  .user-follow-info {
+    margin-top: 6px;
+  }
+}
+
+@media (min-width: 840px) {
+  .title-flex {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: start;
+  }
+  .user-follow-info {
+    margin-top: 6px;
+  }
+}
+
+@media (min-width: 1024px) {
+  .box {
+    flex-basis: calc(33.3% - 16px);
+  }
+  .title-flex {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: start;
+  }
+  .user-follow-info {
+    margin-top: 6px;
+  }
+}
+
+@media (min-width: 1100px) {
+  .box {
+    flex-basis: calc(25% - 18px);
+  }
+}
       </style>
       <?php include_once "navbar.html" ?>
       <?
@@ -86,6 +271,7 @@ require_once "../admin/connByAlan.php";
  
 
   <!-- coba tarik dari database ke admin page -->
+  <div class="wrap">
 <?php
 
 include 'koneksi.php'; // Using database connection file here
@@ -96,21 +282,21 @@ while($data = mysqli_fetch_array($records))
 {
 ?>
 <!-- pembatas -->
-<div class="wrap">
+
   <div class="box">
     <div class="box-top"> 
-      <img class="box-image" <?php echo "<img src='../admin/foto/" . $data["foto"] . "'>"; ?> >>
+      <img class="box-image" <?php echo "<img src='../admin/foto/" . $data["foto"] . "'>"; ?> 
       <div class="title-flex">
         <h3 class="box-title"><?php echo $data['nama']; ?></h3>
         <p class="user-follow-info"><?php echo $data['namaJabatan']; ?></p>
       </div>
-      <p class="description"><?php echo $data['biodata']; ?></p>
     </div>
   </div>
 
 <?php
 }
 ?>
+</div>
 
 
 <?php mysqli_close($sambung); // Close connection ?>
