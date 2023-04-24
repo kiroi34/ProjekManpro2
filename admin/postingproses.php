5c1c6e71c71a648a4fb56168c27918b044859e97
Angelica Agatha <125953295+angelicagatha@users.noreply.github.com>
2023-04-24 13:35:50 +0700

    posting news

--- admin/postingproses.php
+++ admin/postingproses.php
@@ -40,35 +40,59 @@
                 if ($kategori == ''){
                     echo 'Kategori invalid <br>';
                 }
+
                 if ($judul == '') {
                     echo 'Judul invalid <br>';
                 }
+
                 if ($konten == '') {
                     echo 'Konten invalid <br>';
                 }
+                
                 if ($tanggal == 00-00-0000) {
                     echo 'Tanggal invalid<br>';
                 }
+
                 if($_FILES == null) {
                     echo 'File invalid <br>';
                 }
+
                 echo '<br><br><h3>Silahkan mengulangi input</h3>';
                 echo '<a href="postingNews.php"><h3>Back</h3></a>';
 
             } else {
                 
                 if ($extention == 'png' || $extention == 'jpg' || $extention == 'jpeg' || $extention == 'PNG' || $extention == 'JPG' || $extention == 'JPEG'){
+                    
                     move_uploaded_file($tmp_file, $imgStorePath);
                     $query = "INSERT INTO berita (kategori, judul, konten, tanggal, gambar)
-                        VALUES ('". $kategori."', '". $judul."', '". $konten ."', '". $tanggal ."', '" . $gambar . "')";
+                        VALUES ('". $kategori."', '". $judul ."', '". $konten ."', '". $tanggal ."', '". $gambar ."')";
                     $sambung->query($query);
-                    echo '<p>HORE BERHASIL</p>';
+
+                    echo "<script>";
+                        echo "alert('Selamat, Data berhasil diupload!');";
+                        echo "window.location.href = document.referrer;";
+                        echo "function clearForm() {";
+                        echo "    var form = document.getElementById('formulir');";
+                        echo "    form.reset();";
+                        echo "}";
+                        echo "clearForm();";
+                    echo "</script>";
+                    
                 } else {
                     echo "extention tidak sesuai";
                 }
-
-                
             }
         ?>
+        
+        <script>
+            alert("Selamat, Data berhasil diupload!");
+            window.location.href = document.referrer;
+            function clearForm() {
+                var form = document.getElementById("formulir");
+                form.reset();
+            }
+            clearForm();
+        </script>
     </body>
 </html>
