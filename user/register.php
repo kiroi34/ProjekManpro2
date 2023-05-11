00a475a066e618cc96395ef749a7afa6ebab845d
Cory Livira <corylivira36@gmail.com>
2023-05-12 01:39:47 +0700

    Update register.php

--- user/register.php
+++ user/register.php
@@ -1,40 +1,63 @@
 <!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body, html {
-  height: 100%;
-  font-family: 'Poppins';font-size: 18px;
-}
+<html lang="en">
+   <headd>
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
 
-* {
-  box-sizing: border-box;
-}
+    <body>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="csrf-param" content="_csrf">
+      <meta name="csrf-token" content="eFnI7r69sRmjJ4WVIYs1pCAeux2VJAtUpBDAhbT2rLNKbZuk8YTQfdFi7e8R2gTFelb3a_RGUxf7fqrgxLH9ig==">
+      <link href="https://gmschurch.azureedge.net/gmsbaratlandingpagedata/css/bootstrap.min.css" rel="stylesheet">
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" rel="stylesheet">
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.4.6/css/flag-icon.min.css" rel="stylesheet">
+      <style>
 
+        body{
+          font-family: 'Poppins';font-size: 18px;
 
-/* Add styles to the form container */
-.container {
-  Top : 10%;
-  Left : 38%;
-  position: absolute;
-  right: 0;
-  margin: 20px;
-  max-width: 300px;
-  padding: 16px;
-  background-color:  #07012d;
-}
+        }
 
-/* Full-width input fields */
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 15px;
-  margin: 5px 0 22px 0;
-  border: none;
-  background: #c1c1c1;
-}
+        @media (min-width : 768px) {
+          #countdownWrapper {
+            margin-top: 0;
+            position: absolute;
+            right: 20px;
+            padding: 5px;
+            box-sizing: content-box;
+            top: 0;
+            color: black;
+            background-color: rgba(111, 111, 111, 0.5);
+            border-radius: 5px;
+            font-size: 0.7em;
+          }
+    
+          #countdownWrapper .row {
+            margin-left: 0;
+            margin-right: 0;
+          }
+        }
+        #btnDonateWCC {
+          padding-top: 8px;
+          padding-bottom: 8px;
+        }
+        .form-group.error .help-block {
+          color: #B30000;
+          text-align: left;
+        }
+        .form-group.error input {
+          border-color: #B30000;
+        }
+        /* Full-width input fields */
+        input[type=text], input[type=password] {
+        width: 100%;
+        padding: 15px;
+        margin: 5px 0 22px 0;
+        border: none;
+        background: #c1c1c1;
+        }
 
-input[type=text]:focus, input[type=password]:focus {
+  input[type=text]:focus, input[type=password]:focus {
   background-color: #ddd;
   outline: none;
 }
@@ -69,28 +92,135 @@ input[type=text]:focus, input[type=password]:focus {
   background-repeat: no-repeat;
   background-size: cover;
 }
-</style>
-</head>
-<body>
-<div class="bg-image"> </div>
-  <form action="inputnewuser.php" method = "post" class="container">
-    <h1 style = "color : #f4f2ff">Register</h1>
+</style> 
+      
+      </nav>
+      <div id="mainContainer">
+        <div id="streamingContainer" class="container" style="max-width:100%; padding:0;">    
+    <div style="padding-left:0; padding-right:0">
+        <div style="width:100%;" class="mr-auto ml-auto">
+            <div style="background-image:url('/images/form.jpg'); background-repeat:no-repeat; background-position:inherit; background-size:cover; background-color:#1C1C1C; padding:15px;">
+                <div class="cginfo-custom mr-auto ml-auto" style="font-size:35px; margin-top:50px; color:white; max-width:900px; text-align: center;">
+                    SELAMAT DATANG dan WELCOME HOME               
+                </div>
+                <div class="mr-auto ml-auto" style="font-size:14px; margin-left: 50px; color:#a5a5a5; max-width:770px; text-align: center;">
+                    Silahkan isi form dibawah ini sesuai dengan tujuan anda. Tuhan Yesus Memberkati.              
+                </div>
+            </div>
 
-    <label for="nama"><b style = "color : #f4f2ff">Name</b></label>
-    <input type="text" placeholder="Masukkan Nama" name="nama" required>
-    <label for="notelp"><b style = "color : #f4f2ff">Phone Number</b></label>
-    <input type="text" placeholder="Masukkan No Hp" name="notelp" required>
-    <label for="nama"><b style = "color : #f4f2ff">Email</b></label>
-    <input type="text" placeholder="Masukkan email " name="email" required>
-    <label for="nama"><b style = "color : #f4f2ff">Alamat</b></label>
-    <input type="text" placeholder="Masukkan alamat " name="alamat" required>
-    <label for="password"><b style = "color : #f4f2ff">Password</b></label>
-    <input type="password" placeholder="Masukkan Password" name="password" required>
-    <label for="confpassword"><b style = "color : #f4f2ff">Confirmation Password</b></label>
-    <input type="password" placeholder="Konfirmasi Password" name="confpassword" required>
-    <div style="size: 20px;">
-    <p style= "font-size: 75%"><span style = "color : #f4f2ff">Sudah punya akun?<a href="keluar.php"> <span style = "color : #f59d06">Login </pan> </a> </p>
-        <button type="submit" class="btn"><a href= "keluar.php">Register</button>
+            <div class="mt-4" style="max-width:800px; margin:auto;">
+                <div class="streaming-sydney-revival-service">
+                <form id="w0" class="submit-form" action="/form/submit-iftn" method="post">
+    <input type="hidden" name="_csrf" value="jdwIbDQwYoscgf9_f2BT2xmBiioJIpPYQPnC3Gx8NyS_6FsmewkD727ElwVPMWK6Q8nGXGhAy5sfl6i5HDtmHQ==">       
+            <div class="container" style="margin-bottom:20px;">    
+            <div style="text-align:left;">
+            <div class="form-group field-congregationalserviceform-checked_form">
+              <div id="congregationalserviceform-checked_form" aria-invalid="false"><label id="checkmarklabel0" class="checkmark-custom">
+                
+<body>
+<!-- Bikin Background Form -->
+<div id="pelayanan" class="container" style="margin-bottom: 20px;">
+<div style="background-image:url('https://tse2.mm.bing.net/th?id=OIP.BccjVtHxFhSRb3g37MEpdgHaFj&pid=Api&P=0'); background-repeat:no-repeat; background-position:center; background-size:cover; background-color:#1C1C1C; border-radius:10px; padding:20px;">
+<!-- FORM 2 : REGISTRASI JEMAAT -->
+<div style="font-size:18px; margin-bottom:15px; color:white; text-align:center; background-color: black;"> <b>Registrasi Jemaat </b>
+</div>
+<div id="form" >
+    <div class="container" style="margin-bottom:20px;">
+        <div style="background-color:white; border-radius:10px; padding:20px;">
+
+            <div style="font-size:12px; margin-top:-3px; margin-bottom:20px; color:#636363; text-align:left; font-size: medium;">
+                <b>Silahkan isi form berikut ini dengan benar dan sesuai : </b>
+            </div>
+
+<input type="hidden" id="congregationalserviceform-tag" name="CongregationalServiceForm[tag]">     
+<!-- Nama -->
+<div class="form-group field-congregationalserviceform-name required">
+<input type="text" id="congregationalserviceform-name" class="form-control" name="CongregationalServiceForm[name]" placeholder="Nama" aria-required="true">
+<div class="help-block"></div>
+</div>
+<div style="display:flex;">
+<div style="width:100%;">
+
+<!-- Jenis Kelamin/Gender -->
+<div class="form-group field-congregationalserviceform-gender required">
+<input type="hidden" name="CongregationalServiceForm[gender]" value=""><div id="congregationalserviceform-gender" labeloptions="{&quot;style&quot;:&quot;display:inline;&quot;}" role="radiogroup" aria-required="true">
+<label class="checkbox-label-custom"><input type="radio" id="gender0" name="CongregationalServiceForm[gender]" value="M" tabindex="3">
+<span class="checkbox-span" style="margin-left:12px; margin-top:-15px;">Laki-Laki</span></label>&nbsp;&nbsp;&nbsp;<label class="checkbox-label-custom">
+<input type="radio" id="gender1" name="CongregationalServiceForm[gender]" value="F" tabindex="3">
+<span class="checkbox-span" style="margin-left:12px; margin-top:-15px;">Perempuan</span></label></div>
+
+<div class="help-block"></div>
+</div>                                        
+</div>
+<div style="width:120px;">
+<div class="form-group field-ageField required">
+
+<!-- Umur -->
+<input type="tel" id="ageField" class="form-control" name="CongregationalServiceForm[age]" placeholder="Umur" aria-required="true">
+
+<div class="help-block"></div>
+</div>                                        
+</div>
+</div>
+
+<!-- Nomor Tlepon -->
+<div class="form-group field-phoneField">
+<input type="tel" id="phoneField" class="form-control" name="CongregationalServiceForm[phone_number]" maxlength="20" placeholder="Nomor Telepon">
+
+<div class="help-block"></div>
+</div>           
+<!-- Email -->
+<div class="form-group field-congregationalserviceform-email">
+<input type="text" id="congregationalserviceform-email" class="form-control" name="CongregationalServiceForm[email]" maxlength="150" placeholder="Email">
+<div class="help-block"></div>
+</div>      
+
+<!-- Kota -->
+<div class="form-group field-congregationalserviceform-city">
+    <input type="text" id="congregationalserviceform-city" class="form-control" name="CongregationalServiceForm[city]" maxlength="150" placeholder="Kota">
+    
+<div class="help-block"></div>
+</div>
+
+<!-- Alamat -->
+<div class="form-group field-congregationalserviceform-address">
+    <input type="text" id="congregationalserviceform-address" class="form-control" name="CongregationalServiceForm[address]" maxlength="250" placeholder="Alamat Domisili">
+    
+<div class="help-block">
+</div>
+</div> 
+
+<!-- Checkbox mau tergabung sebagai jemaat gereja  -->
+<label class="checkbox-label-custom">
+    <input id="checkgabungjemaat" type="checkbox" class="check-join-cg check-checkmark" value="1" tabindex="3">
+    <span class="checkbox-span" style="margin-left:12px; margin-top:-15px; background-color:transparent;"></span>
+    </label>
+    <span style="margin-left:20px; top:-2px; right:25px;position:relative; font-size: medium; color: black;">Saya punya kerinduan untuk menjadi Jemaat di Gereja</span>
+<br>
+
+<br>  
+   
+</div>
+<br>
+<!-- Konfirmasi User -->
+<div id="terms1Wrapper" style="display: block;">
+<div style="font-size:12px; margin:0px 0px 0px 0px; color:black; opacity: 60%; text-align: center;">
+    <b>Dengan mengirimkan form ini, saya bersungguh-sungguh dan punya kerinduan untuk Tuhan<br>
+        serta siap untuk melanjutkan ke langkah selanjutnya dibawah naungan Gereja.</b>
+</div>
+<br>
+<div style="size: 20px;">
+    <p style= "font-size: 70%;"><span style = "color : black">Sudah punya akun?<a href="keluar.php"> <span style = "color : red"><b>Login<b> </pan> </a> </p>
     </div>
+            
+<!-- Submit Button nanti masuk ke login juga harusnya-->
+    <div class="form-group" style="margin-top:25px;margin-bottom:25px;">
+    <button type="submit" id="submitButton" class="btn btn-primary btn-block" style="font-size:18px; letter-spacing:4px; border:0; background-color:black;"><a href="keluar.php">Daftar</button> </a> </pan>                               
+</div>
+    </div>
+</div>
+
+    </body>
   </form>
 </body>
+</head>
