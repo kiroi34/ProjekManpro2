ffb3e6e0783fafe748b8396f1adf851949179a64
Alan Satria <alansatria6@gmail.com>
2023-05-29 22:08:06 +0700

    revisi3

--- user/registerAkunDatabase.php
+++ user/registerAkunDatabase.php
@@ -12,7 +12,6 @@ $email = $_POST['email'];
 $pass = $_POST['password'];
 $c_pass = $_POST['cpassword'];
 $kota = $_POST['kota'];
-$provinsi = $_POST['provinsi'];
 $alamat = $_POST['alamat'];
 $idgereja = $_SESSION['gereja'];
 
@@ -20,8 +19,8 @@ if ( $pass != $c_pass) {
     echo "<script>alert('PASSWORD HARUS SAMA'); window.location.href = 'register.php';</script>";
 }else {
     if (isset($_POST['daftar'])) { 
-        mysqli_query($sambung, "INSERT INTO akunjemaat ( `namaLengkap`, `jenisKelamin`, `tanggalLahir`, `tempatLahir`, `umur`, `nomorTelepon`, `email`, `password`, `kota`,`provinsi`, `alamatDomisili`,`idgereja`) 
-        VALUES ('$nama','$jk','$tanggalLahir','$tempatLahir','$umur','$nomorTelpon','$email',MD5('$pass'),'$kota','$provinsi','$alamat','$idgereja')");
+        mysqli_query($sambung, "INSERT INTO akunjemaat ( `namaLengkap`, `jenisKelamin`, `tanggalLahir`, `tempatLahir`, `umur`, `nomorTelepon`, `email`, `password`, `kota`, `alamatDomisili`,`idgereja`) 
+        VALUES ('$nama','$jk','$tanggalLahir','$tempatLahir','$umur','$nomorTelpon','$email',MD5('$pass'),'$kota','$alamat','$idgereja')");
         echo "<script>alert('Pendaftaran Akun Berhasil, Silahkan Login'); window.location.href = 'keluar.php';</script>";
     } else {
         echo "<script>alert('Gagal mendaftar!'); window.location.href = 'register.php';</script>";
