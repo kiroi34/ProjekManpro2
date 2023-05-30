1cbf8312b5af8ec5a07c877bf3846928497276a0
fellenwennesa <fellenwennesa270103@gmail.com>
2023-05-30 13:48:40 +0700

    imel

--- masteradmin/php/hapus.php
+++ masteradmin/php/hapus.php
@@ -6,4 +6,25 @@
     $sql = "DELETE FROM gereja
     WHERE idgereja = ".$id;
     $stmt = $sambung->query($sql);
+
+    $sql2 = "SELECT * FROM gereja WHERE idgereja = ".$id;
+    $stmt2 = $sambung->query($sql2);
+    foreach ($stmt2 as $data) {
+    require_once "../Other/functions.php";
+    $subject = "Gereja telah dihapus dari sistem!";
+    $body= '<!DOCTYPE html>
+            <html lang="en">
+            <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+            <title>Gereja telah dihapus dari sistem</title>
+            </head>
+            <body style="color:black">
+                <strong>Website gereja anda sudah terhapus dari sistem!</strong><br>
+            </body>
+            </html>';
+    sendEmail($subject,$body,$data['email']);
+    }
 ?>
--- masteradmin/php/tolak.php
+++ masteradmin/php/tolak.php
@@ -20,10 +20,10 @@
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
-            <title>endaftaran Gereja Ditolak!</title>
+            <title>Pendaftaran Gereja Ditolak!</title>
             </head>
             <body style="color:black">
-                <strong>Mohon maaf, karena berbagai pertimbangan, konfirmasi dari pendaftaran gereja anda ditolak!</strong><br>
+                <strong>Mohon maaf, karena berbagai pertimbangan, pendaftaran gereja anda ditolak!</strong><br>
             </body>
             </html>';
     sendEmail($subject,$body,$data['email']);
